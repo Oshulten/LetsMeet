09ae4371691c05576faf220b086b6a86c49aebec
Oscar Hultén <oshulten@gmail.com>
2024-09-02 23:28:45 +0200

    feat: restore changes

--- Frontend/src/components/GoogleMap.tsx
+++ Frontend/src/components/GoogleMap.tsx
@@ -1,54 +1,40 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { useQuery } from '@tanstack/react-query';
 import { AdvancedMarker, APIProvider, Map, Pin } from '@vis.gl/react-google-maps';
-import { getGoogleMapsApiKey } from '../api/endpoints';
 import getPosition from '../utilities/getPosition';
 import useSignalRLocations from '../hooks/useSignalRLocations';
 
 export default function GoogleMap() {
-    const apiKeyQuery = useQuery({ queryKey: ["googleApiKey"], queryFn: getGoogleMapsApiKey });
     const geolocationQuery = useQuery({ queryKey: ["geolocation"], queryFn: () => getPosition() });
     const locations = useSignalRLocations();
 
     console.log(`locations: ${JSON.stringify(locations)}`)
 
-    switch (apiKeyQuery.status) {
-        case "pending":
-            return <p>Fetching Google Maps api key...</p>
-
-        case "error":
-            return <p>Google Maps api key could not be fetched</p>
-
-        case "success":
-            if (apiKeyQuery.data) {
-                if (!geolocationQuery.data?.coords) {
-                    return <span className="loading loading-ring loading-lg"></span>
-                }
-                const location: google.maps.LatLngLiteral = {
-                    lat: geolocationQuery.data.coords.latitude,
-                    lng: geolocationQuery.data.coords.longitude
-                }
-
-
-
-                return (
-                    <>
-                        <APIProvider apiKey={apiKeyQuery.data}>
-                            <Map
-                                style={{ width: '100vw', height: '100vh' }}
-                                defaultCenter={location}
-                                defaultZoom={15}
-                                gestureHandling={'greedy'}
-                                disableDefaultUI={true}
-                                mapId={"LetsMeetMap"}>
-                                <AdvancedMarker
-                                    position={location}>
-                                    <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
-                                </AdvancedMarker>
-                            </Map>
-                        </APIProvider>
-                    </>
-                );
-            }
+    if (!geolocationQuery.data?.coords) {
+        return <span className="loading loading-ring loading-lg"></span>
     }
+
+    const location: google.maps.LatLngLiteral = {
+        lat: geolocationQuery.data.coords.latitude,
+        lng: geolocationQuery.data.coords.longitude
+    }
+
+    return (
+        <>
+            <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+                <Map
+                    style={{ width: '100vw', height: '100vh' }}
+                    defaultCenter={location}
+                    defaultZoom={15}
+                    gestureHandling={'greedy'}
+                    disableDefaultUI={true}
+                    mapId={"LetsMeetMap"}>
+                    <AdvancedMarker
+                        position={location}>
+                        <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+                    </AdvancedMarker>
+                </Map>
+            </APIProvider>
+        </>
+    );
 }
