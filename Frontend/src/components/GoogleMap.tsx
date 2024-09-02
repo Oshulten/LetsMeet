9491f7e2ad305235724ab7ac6546332c796c1eb1
Oscar Hultén <oshulten@gmail.com>
2024-09-03 00:19:56 +0200

    fix: lost features

--- Frontend/src/components/GoogleMap.tsx
+++ Frontend/src/components/GoogleMap.tsx
@@ -1,39 +1,31 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { useQuery } from '@tanstack/react-query';
-import { AdvancedMarker, APIProvider, Map, Pin } from '@vis.gl/react-google-maps';
-import getPosition from '../utilities/getPosition';
-import useSignalRLocations from '../hooks/useSignalRLocations';
+import { AdvancedMarker, APIProvider, Map, MapMouseEvent, Pin } from '@vis.gl/react-google-maps';
+import { useState } from 'react';
 
-export default function GoogleMap() {
-    const geolocationQuery = useQuery({ queryKey: ["geolocation"], queryFn: () => getPosition() });
-    const locations = useSignalRLocations();
+interface Props {
+    defaultLocation: google.maps.LatLngLiteral
+}
 
-    console.log(`locations: ${JSON.stringify(locations)}`)
+export default function GoogleMap({ defaultLocation }: Props) {
+    const [currentLocation, setCurrentLocation] = useState<google.maps.LatLngLiteral | null>(defaultLocation);
 
-    if (!geolocationQuery.data?.coords) {
-        return <span className="loading loading-ring loading-lg"></span>
-    }
-    const location: google.maps.LatLngLiteral = {
-        lat: geolocationQuery.data.coords.latitude,
-        lng: geolocationQuery.data.coords.longitude
+    const handleContextmenu = (e: MapMouseEvent) => {
+        setCurrentLocation(e.detail.latLng);
     }
 
     return (
         <>
-            <APIProvider
-                apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
-                region="SE">
+            <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
                 <Map
                     style={{ width: '100vw', height: '100vh' }}
-                    defaultCenter={location}
+                    defaultCenter={defaultLocation}
                     defaultZoom={15}
                     gestureHandling={'greedy'}
                     disableDefaultUI={true}
                     mapId={"LetsMeetMap"}
-                    reuseMaps={true}
-                    colorScheme={'DARK'}>
+                    onContextmenu={handleContextmenu}>
                     <AdvancedMarker
-                        position={location}>
+                        position={currentLocation ?? defaultLocation}>
                         <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
                     </AdvancedMarker>
                 </Map>
