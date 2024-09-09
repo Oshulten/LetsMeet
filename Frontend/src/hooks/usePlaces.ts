ae3ae5703b454ac56b3f2b244aea68383c1ace7e
Oscar Hultén <oshulten@gmail.com>
2024-09-09 20:19:12 +0200

    feat: suggestMeetingPlaces

--- Frontend/src/hooks/usePlaces.ts
+++ Frontend/src/hooks/usePlaces.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { User } from "../types/types";
+import { mean, max } from 'mathjs/number'
+import haversine from 'haversine-distance';
 
 export default function usePlaces() {
     const [library, setLibrary] = useState<google.maps.PlacesLibrary>();
@@ -12,10 +14,41 @@ export default function usePlaces() {
 
         if (!library) {
             loadLibraries();
+            console.log("loading libraries");
         }
     });
 
     const suggestMeetingPlaces = async (users: User[]) => {
+        if (!library) return;
+
+        const meanLocation: google.maps.LatLngLiteral = {
+            lat: mean(users.map(user => user.location.lat)),
+            lng: mean(users.map(user => user.location.lng)),
+        };
+
+
+        const maxDistanceFromMeanLocation = max(users.map(user =>
+            haversine(user.location, meanLocation)
+        ));
+
+        const request = {
+            // required parameters
+            fields: ['displayName', 'location', 'id'],
+            locationRestriction: {
+                center: meanLocation,
+                radius: maxDistanceFromMeanLocation + 50,
+            },
+            // optional parameters
+            // includedPrimaryTypes: ['restaurant'],
+            maxResultCount: 5,
+            rankPreference: 'DISTANCE',
+            // language: 'en-US',
+            // region: 'us',
+        } as google.maps.places.SearchNearbyRequest;
+
+        const { places } = await library.Place.searchNearby(request);
+
+        console.log(places);
     }
 
     return {
